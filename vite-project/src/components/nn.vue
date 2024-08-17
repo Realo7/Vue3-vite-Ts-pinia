46470eb8d22609e223419451f462f76d0d73dc84
PC-REALO\realo <382526563@qq.com>
2024-08-18 00:19:11 +0800

    no message

---
+++ vite-project/src/components/nn.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container"></div>
+</template>
+<script setup lang="ts">
+import { ref, reactive } from 'vue'
+import type { Ref } from 'vue' 
+</script>
+<style lang="scss" scoped>
+.container {
+  display: flex;
+}
+</style>
